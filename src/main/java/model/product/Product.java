07ac74f0e176c674bbec6b02fee430ebe2b3c469
Khusan <erkinovhusan1@gmail.com>
2021-12-09 08:31:40 +0500

    model is changed

--- src/main/java/model/product/Product.java
+++ src/main/java/model/product/Product.java
@@ -2,5 +2,12 @@ package model.product;
 
 import model.BaseModel;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Product extends BaseModel {
+    private String title;
+    private String description;
+    private BigDecimal price;
 }
