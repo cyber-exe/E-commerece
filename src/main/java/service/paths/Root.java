d62d3de0d6b294c289fdbde825780b80c36b1a70
Khusan <erkinovhusan1@gmail.com>
2021-12-13 17:49:04 +0500

    Added some codes to TgBot7

--- src/main/java/service/paths/Root.java
+++ src/main/java/service/paths/Root.java
@@ -1,8 +1,13 @@
 package service.paths;
 
 public  interface Root {
+    //Behzod
 //    String basePath = "C:\\Users\\delta\\Documents\\E-commerce\\src\\main\\resources\\";
+    //Husan
     String basePath = "C:\\Users\\User\\IdeaProjects\\E-commerece\\src\\main\\resources\\";
+    //Alisher
+    //String basePath = "D:\\PDP-Unicorn\\JavaOOP\\ProjectsFromOthers\\E-commerece";
+
     String buyersPath = basePath + "buyers.json";
     String cardsPath = basePath + "cards.json";
     String sellersPath = basePath + "sellers.json";
