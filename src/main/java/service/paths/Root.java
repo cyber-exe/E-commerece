9861dfca40c03e835d7071e2332b805a2584ee7f
Your Name <you@example.com>
2021-12-13 16:52:56 +0500

    Merge remote-tracking branch 'origin/master'

--- src/main/java/service/paths/Root.java
+++ src/main/java/service/paths/Root.java
@@ -1,10 +1,15 @@
 package service.paths;
 
 public  interface Root {
-//    String basePath = "C:\\Users\\delta\\Documents\\E-commerce\\src\\main\\resources\\";
-    String basePath = "C:\\Users\\User\\IdeaProjects\\E-commerece\\src\\main\\resources\\";
 
-    //String basePath = "D:\\PDP-Unicorn\\JavaOOP\\ProjectsFromOthers\\E-commerece\\src\\main\\resources\\";
+    //Behzod
+    //String basePath = "C:\\Users\\delta\\Documents\\E-commerce\\src\\main\\resources\\";
+
+    //Khusan
+    //String basePath = "C:\\Users\\User\\IdeaProjects\\E-commerece\\src\\main\\resources\\";
+
+    //Alisher
+    String basePath = "D:\\PDP-Unicorn\\JavaOOP\\ProjectsFromOthers\\E-commerece\\src\\main\\resources\\";
     String buyersPath = basePath + "buyers.json";
     String cardsPath = basePath + "cards.json";
     String sellersPath = basePath + "sellers.json";
